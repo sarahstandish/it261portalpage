6ca0e21feeedb1c557cc2d24b74fb0d7d93274b0
sarahstandish <z.sisyphusrocks@gmail.com>
2021-01-25 09:19:30 -0800

    added navigation between days

--- website/week3/switch.php
+++ website/week3/switch.php
@@ -101,7 +101,18 @@ echo "<a href='$day[recipe_url]' target='_blank'>" . $day['recipe_name'] . "</a>
         </a>
     </div>
 </div>
-
+<footer>
+<h2>Check out our daily recipes!</h2>
+    <ul>
+        <li><a href="switch.php?today=Sunday">Sunday</a></li>
+        <li><a href="switch.php?today=Monday">Monday</a></li>
+        <li><a href="switch.php?today=Tuesday">Tuesday</a></li>
+        <li><a href="switch.php?today=Wednesday">Wednesday</a></li>
+        <li><a href="switch.php?today=Thursday">Thursday</a></li>
+        <li><a href="switch.php?today=Friday">Friday</a></li>
+        <li><a href="switch.php?today=Saturday">Saturday</a></li>
+    </ul
+</footer>
 <?php
 include '../../includes/footer.php';
 footer();
