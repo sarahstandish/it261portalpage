56db0040f113e2cb0fafa9d994795e2164e4b7e5
sarahstandish <z.sisyphusrocks@gmail.com>
2021-02-04 12:34:15 -0800

    week 5 assignments works in progress

--- website/week5/calculator-days.php
+++ website/week5/calculator-days.php
@@ -6,7 +6,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Syne:wght@400;500&display=swap" rel="stylesheet"> 
     <title>Calculator</title>
     <style>
-                * {
+           * {
             color: var(--text-color)
         }
         h2 {
@@ -17,43 +17,54 @@
         }
         label {
             font-family: var(--body-font);
+            display: block;
+            margin: 5px;
         }
         form {
         width: 400px;
         margin: 0 auto;
-    }
+        }
+        input {
+            /* display: block; */
+            color: var(--background-color)
+        }
 
-    label {
-        /* display: block; */
-        margin: 5px;
-        padding: 10px;
-    }
+        h1, h2 {
+            text-align: center;
+            margin: 20px;
+        }
 
-    input {
-        /* display: block; */
-        margin-bottom: 10px;
-        padding: 10px;
-        color: var(--background-color)
-    }
+        fieldset {
+            border: 5px solid var(--accent-color-1);
+            padding: 10px;
+        }
 
-    h1, h2 {
-        text-align: center;
-        margin: 20px;
-    }
+        .response {
+            margin: 0 auto;
+            text-align: center;
+            padding: 10px;
+        }
 
-    fieldset {
-        border: 5px solid var(--accent-color-1);
-        padding: 10px;
-    }
+        response p {
+            margin: 10px;
+        }
 
-    .response {
-        margin: 0 auto;
-        text-align: center;
-    }
+        input[type=text] {
+            width: 100%;
+            margin-bottom: 10px;
+        }
 
-    response p {
-        margin: 10px;
-    }
+        input[type=submit] {
+            display: block;
+            margin: 10px 0px;
+            min-width: 75px;
+        }
+        
+        form ul {
+            margin-left: 20px;
+            list-style-type: none;
+            margin-bottom: 10px;
+        }
     
     </style>
 </head>
@@ -63,34 +74,25 @@
         <fieldset>
             <label for='name'>Your name:</label>
                 <input type='text' name='name'>
-                <br>
             <label for='miles'>How many miles will you drive?</label>
                 <input type='text' name='miles'>
-                <br>
             <label for='miles'>How many hours per day do you want to drive?</label>
                 <input type='text' name='hours'>
-                <br>
             <label>Price per gallon</label>
-                <br>
-                <input type='radio' name='price' value='3'>
-                    <label for='price'>$3.00</label>
-                    <br>
-                <input type='radio' name='price' value='3.5'>
-                    <label for='price'>$3.50</label>
-                    <br>
-                <input type='radio' name='price' value='4'>
-                    <label for='price'>$4.00</label>
-                    <br>
+            <ul>
+                <li><input type='radio' name='price' value='3'>$3.00</li>
+                <li><input type='radio' name='price' value='3.5'>$3.50</li>
+                <li><input type='radio' name='price' value='4'>$4.00</li>
+            </ul>
             <label>Fuel Efficiency</label>
-                <br>
                 <select name='efficiency'>
+                    <option value='NULL'>Select an efficiency</option>
                     <option value='great'>Great</option>
                     <option value='good'>Good</option>
                     <option value='okay'>Okay</option>
                     <option value='bad'>Bad</option>
                     <option value='terrible'>Terrible</option>
                 </select>
-                <br>
             <input type='submit' name='submit' value='Submit'>
             <p><a href="">Reset</a></p>
         </fieldset>
@@ -98,15 +100,15 @@
     <div class='response'>
         <?php
         $efficiency_grades = [
-            'great' => 60,
-            'good' => 50,
-            'okay' => 40,
-            'bad' => 30,
-            'terrible' => 20
+            'great' => 50,
+            'good' => 40,
+            'okay' => 30,
+            'bad' => 20,
+            'terrible' => 10
         ];
 
         if (isset($_POST['submit'])) {
-            if (empty($_POST['price']) || empty($_POST['miles']) || empty($_POST['name'] || empty($_POST['hours']))) {
+            if (empty($_POST['price']) || empty($_POST['miles']) || empty($_POST['name']) || empty($_POST['hours']) || $_POST['efficiency'] == 'NULL') {
                 echo "<p>Please fill out all fields before pressing submit</p>";
             } elseif (!is_numeric($_POST['miles']) || !is_numeric($_POST['hours'])) {
                 echo "<p>Miles and hours must be numbers</p>";
---
+++ weeks/week5/currency3.php
@@ -0,0 +1,131 @@
+<?php
+    function get_rate($currency_symbol) {
+        $currency_url = "https://api.exchangeratesapi.io/latest?base=$currency_symbol";
+
+        $currency_json = file_get_contents($currency_url);
+        $currency_array = json_decode($currency_json, true);
+        $rate = $currency_array['rates']['USD'];
+        return $rate;
+    }    
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Currency 3</title>
+    <link href="/it261/css/styles.css" type="text/css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Syne:wght@400;500&display=swap" rel="stylesheet">
+<style>
+    * {
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
+    }
+    form {
+        max-width: 600px;
+        margin: 20px auto;
+    }
+    fieldset {
+        padding: 10px;
+    }
+    label {
+        display: block;
+    }
+
+    input[type=text], input[type=email] {
+        width: 100%;
+        margin-bottom: 10px;
+    }
+
+    form ul {
+        margin-left: 20px;
+        list-style-type: none;
+        margin-bottom: 10px;
+    }
+    .box {
+        width: 400px;
+        margin: 20px auto;
+        padding: 10px;
+    }
+</style>
+</head>
+<body>
+    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF'])?>" method="post">
+        <fieldset>
+            <label>Name</label>
+                <input type="text" name="name" value="<?php if (isset($_POST['name'])) echo htmlspecialchars($_POST['name'])?>">
+            <label>Email</label>
+                <input type="email" name="email" value="<?php if (isset($_POST['email'])) echo htmlspecialchars($_POST['email'])?>">
+            <label>How much money do you have in foreign currency?</label>
+                <input type="text" name="amount" value="<?php if (isset($_POST['amount'])) echo htmlspecialchars($_POST['amount'])?>">
+            <label>Which currency do you have?</label>
+                <ul>
+                    <li><input type="radio" name="currency" value="Rubles" <?php if (isset($_POST['currency']) && $_POST['currency'] == "Rubles") echo "checked = 'checked'"  ?>>Rubles</li>
+                    <li><input type="radio" name="currency" value="Canadian Dollars" <?php if (isset($_POST['currency']) && $_POST['currency'] == "Canadian Dollars") echo "checked = 'checked'"?>>Canadian Dollars</li>
+                    <li><input type="radio" name="currency" value="Pounds Sterling" <?php if (isset($_POST['currency']) && $_POST['currency'] == "Pounds Sterling") echo "checked = 'checked'"?>>Pounds Sterling</li>
+                    <li><input type="radio" name="currency" value="Euros" <?php if (isset($_POST['currency']) && $_POST['currency'] == "Euros") echo "checked = 'checked'"?>>Euros</li>
+                    <li><input type="radio" name="currency" value="Yen" <?php if (isset($_POST['currency']) && $_POST['currency'] == "Yen") echo "checked = 'checked'"  ?>>Yen</li>
+                </ul>
+            <label>Banking Institution</label>
+                <select name="bank">
+                    <option value="NULL" >Select one:</option>
+                    <option value="Bank of America" <?php if (isset($_POST['bank']) && $_POST['bank'] == "Bank of America") echo "selected"  ?>>Bank of America</option>
+                    <option value="Chase Bank" <?php if (isset($_POST['bank']) && $_POST['bank'] == "Chase Bank") echo "selected"  ?>>Chase</option>
+                    <option value="BECU" <?php if (isset($_POST['bank']) && $_POST['bank'] == "BECU") echo "selected"  ?>>BECU</option>
+                    <option value="your mattress" <?php if (isset($_POST['bank']) && $_POST['bank'] == "your mattress") echo "selected"  ?>>Mattress</option>
+                </select>
+            <input type="submit" value="Convert">
+            <p><a href="">Reset</a></p>
+        </fieldset>
+    </form>
+    <div class="box">
+<?php
+    $symbols = [
+        'Rubles' => 'RUB',
+        'Canadian Dollars' => 'CAD',
+        'Pounds Sterling' => 'GBP',
+        'Euros' => 'EUR',
+        'Yen' => 'JPY'
+    ];
+
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        if (empty($_POST['name'])) {
+            echo "<p>Please fill in your name.</p>";
+        } 
+
+        if (empty($_POST['email'])) {
+            echo "<p>Please fill in your email.</p>";
+        }
+
+        if (empty($_POST['amount']) || !is_numeric($_POST['amount'])) {
+            echo "<p>Please enter an amount. The amount must be a number.</p>";
+        } 
+
+        if (empty($_POST['currency'])) {
+            echo "<p>Please select a currency.</p>";
+        }
+
+        if ($_POST['bank'] == 'NULL') {
+            echo "<p>Please select a bank.</p>";
+        }
+
+        if (isset($_POST['name'],
+            $_POST['email'], 
+            $_POST['amount'], 
+            $_POST['currency']) 
+            && is_numeric($_POST['amount']) 
+            && $_POST['bank'] != 'NULL') {
+                $name = $_POST['name'];
+                $email = $_POST['email'];
+                $amount =  $_POST['amount'];
+                $currency_type = $_POST['currency'];
+                $currency_rate = get_rate($symbols[$currency_type]);
+                $bank = $_POST['bank'];
+                $total = number_format($amount * $currency_rate, 2);
+                echo "<p>Hello, $name.<br>The most recent exchange rate available to convert $currency_type to USD is $currency_rate.<br>You have $$total USD which will be transferred to $bank.<br>You will receive a confirmation email at <a href='mailto:$email'>$email</a>.</p>";
+            }
+    }
+?>
+    </div>
+</body>
+</html>
