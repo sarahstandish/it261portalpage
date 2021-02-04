ddee01bfff4b4c418b62b05082deeff043295658
sarahstandish <z.sisyphusrocks@gmail.com>
2021-02-04 12:51:27 -0800

    removed target="_blank"

--- includes/footer.php
+++ includes/footer.php
@@ -4,13 +4,13 @@ function footer() {
     echo "<footer>
         <ul>
             <li>
-                <a href='https://creativecommons.org/licenses/by-sa/4.0/' target='_blank'>CC BY-SA 4.0</a>
+                <a href='https://creativecommons.org/licenses/by-sa/4.0/'>CC BY-SA 4.0</a>
             </li>
             <li>
-                <a href='http://validator.w3.org/nu/?doc=$page' target='_blank'>HTML VALID</a>
+                <a href='http://validator.w3.org/nu/?doc=$page'>HTML VALID</a>
             </li>
             <li>
-                <a href='https://jigsaw.w3.org/css-validator/validator?uri=$page' target='_blank'>CSS VALID</a>
+                <a href='https://jigsaw.w3.org/css-validator/validator?uri=$page'>CSS VALID</a>
             </li>
         </ul>
     </footer>";
