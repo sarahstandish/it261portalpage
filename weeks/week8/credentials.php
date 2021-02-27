58a017f3eee4e66c6d962bacd5fb9b0cfdaec964
sarahstandish <z.sisyphusrocks@gmail.com>
2021-02-27 10:01:11 -0800

    added local database connection

--- weeks/week8/credentials.php
+++ weeks/week8/credentials.php
@@ -1,8 +1,16 @@
 <?php 
 
-define('DB_NAME', 'dbpbcm4xgtb25a');
 define('DB_HOST', 'localhost');
-define('DB_USER', 'uttdq9rztx97y');
-define('DB_PASSWORD', 'drwt7uhinbyg');
+
+//siteground
+// define('DB_NAME', 'dbpbcm4xgtb25a');
+// define('DB_USER', 'uttdq9rztx97y');
+// define('DB_PASSWORD', 'drwt7uhinbyg');
+
+//mamp
+define('DB_NAME', 'it261');
+define('DB_USER', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_PORT', 3306)
 
 ?>
